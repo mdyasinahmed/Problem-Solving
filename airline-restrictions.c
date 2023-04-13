cbbfc69c37c0961cd18df9eab3796dfb50c0c0df
mdyasinahmed <yasinahmed7875@gmail.com>
2023-04-14 05:02:25 +0600

    airline-restrictions problem solved

--- airline-restrictions.c
+++ airline-restrictions.c
@@ -2,6 +2,15 @@
 
 int main(void) {
 	
+    int t, a, b, c, d, e;
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+
+        if((a + b <= d && c <= e) || (b + c <= d && a <= e) || (c + a <= d && b <= e)) printf("YES\n");
+        else printf("NO\n");
+    }
     
 	return 0;
 }

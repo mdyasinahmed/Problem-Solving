707326e60c8bc3c2d771f93aad84712c873dd3b4
mdyasinahmed <yasinahmed7875@gmail.com>
2023-04-14 02:11:20 +0600

    good turn problem solved

--- good-turn.c
+++ good-turn.c
@@ -2,6 +2,18 @@
 
 int main(void) {
 	
-    
+    int t, x, y;
+    scanf("%d", &t);
+
+    while(t--)
+    {
+        scanf("%d %d", &x, &y);
+        int sum =(x+y);
+
+        if(sum>6) printf("YES\n");
+        else printf("NO\n");
+
+    }
+
 	return 0;
 }

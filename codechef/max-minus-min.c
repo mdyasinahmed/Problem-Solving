04aa2dd26c1f349ca77593230105b7373ba790ee
mdyasinahmed <yasinahmed7875@gmail.com>
2023-04-15 19:50:09 +0600

    max-minus-min problem solved

---
+++ codechef/max-minus-min.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+    int t, a, b, c;
+    scanf("%d", &t);
+
+    while(t--)
+    {
+        scanf("%d %d %d", &a, &b, &c);
+        int max = a > b ? (a > c ? a : c) : (b > c ? b : c);
+        int min = a < b ? (a < c ? a : c) : (b < c ? b : c);
+        
+        printf("%d\n", (max-min));
+    }
+    
+    
+    return 0;
+}
+

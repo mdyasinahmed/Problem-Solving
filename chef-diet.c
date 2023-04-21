a3ec60622c95c06b42a1e552302952f25178c32d
mdyasinahmed <yasinahmed7875@gmail.com>
2023-04-21 12:28:12 +0600

    chef-diet unsolved code updated

--- chef-diet.c
+++ chef-diet.c
@@ -5,32 +5,34 @@ int main()
     int t, n, k, i, j, save=0, unableDay;
     scanf("%d", &t);
     int arr[n];
-    for(i = 0; i < t; i++)
+    for(i = 1; i <= t; i++)
     {
         scanf("%d %d", &n, &k);
-        for(j = 0; j < n; j++)
+        for(j = 1; j <= n; j++)
         {
             scanf("%d", &arr[j]);
             if(arr[j]==k)
             {
-                save += arr[j];
+                save += k-arr[j];
             }
             if(arr[j]>k)
             {
                 save += arr[j]-k;
             }
-            else{
+            if(arr[j]<k)
+            {
+                save = save - (k-arr[j]);
                 unableDay = arr[j];
             }
         }
-        if(save>=k)
+    }
+    if(save>=k)
         {
             printf("YES\n");
         }
         else{
             printf("NO %d\n", unableDay);
         }
-    }
 
     
 

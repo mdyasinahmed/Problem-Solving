db110e9953175ddb878da895f8d66537a887692a
mdyasinahmed <yasinahmed7875@gmail.com>
2023-04-22 23:16:52 +0600

    neflix problem solved

--- (cc-493)netflix.cpp
+++ (cc-493)netflix.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	// your code goes here
+int main(){
+    int t, a, b, c, x;
+    cin>>t;
+
+    while(t--){
+        cin>>a >>b >>c >>x;
+        if(a+b>=x || b+c>=x || c+a>=x){
+            cout<<"YES"<<endl;
+        }else{
+            cout<<"NO"<<endl;
+        }
+    }
+    
 	return 0;
 }

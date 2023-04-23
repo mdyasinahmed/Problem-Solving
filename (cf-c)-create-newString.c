5b7ddefb1827fd112be84df5d8e340f48410e1e3
mdyasinahmed <yasinahmed7875@gmail.com>
2023-04-23 19:58:58 +0600

    new string problem solved code added

--- (cf-c)-create-newString.c
+++ (cf-c)-create-newString.c
@@ -10,7 +10,12 @@ int main()
     gets(str1);
     gets(str2);
 
-    len1 = str
+    len1 = strlen(str1);
+    len2 = strlen(str2);
+
+    printf("%d %d\n", len1, len2);
+    printf("%s %s\n", str1, str2);
+
 
     return 0;
 }

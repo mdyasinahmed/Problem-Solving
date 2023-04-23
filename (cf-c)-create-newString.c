7dd08eddd8099fe08ab3cdfcd51d75793a7d63d0
mdyasinahmed <yasinahmed7875@gmail.com>
2023-04-23 13:01:24 +0600

    create new string problem code file added

---
+++ (cf-c)-create-newString.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str1[1000];
+    char str2[1000];
+    int len1, len2;
+
+    gets(str1);
+    gets(str2);
+
+    len1 = str
+
+    return 0;
+}

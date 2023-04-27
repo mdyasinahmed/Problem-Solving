b5227c3b544a97866bf231186d2b3e42eb2d1d2f
mdyasinahmed <yasinahmed7875@gmail.com>
2023-04-28 00:43:25 +0600

    Programming languages problem solved

--- (cc- LTIME99) - programming-languages.c
+++ (cc- LTIME99) - programming-languages.c
@@ -8,7 +8,19 @@ int main(void)
     while(t--)
     {
         scanf("%d %d %d %d %d %d", &a, &b, &a1, &b1, &a2, &b2);
-
+        
+        if((a==a1 || a==b1) && (b==a1 || b==b1))
+        {
+            printf("1\n");
+        }
+        else if((a==a2 || a==b2) && (b==a2 || b==b2))
+        {
+            printf("2\n");
+        }
+        else
+        {
+            printf("0\n");
+        }
         
     }
 	

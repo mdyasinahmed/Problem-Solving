c1b267c1e57cc5fa2e0aeccfd8d75ec76b906dad
mdyasinahmed <yasinahmed7875@gmail.com>
2023-04-27 23:14:55 +0600

    atm machine unsolved code updated

--- (cc-COOK98) - atm-machine.c
+++ (cc-COOK98) - atm-machine.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
+#define s 1000
 
 int main(void) 
 {
-	int t, n, k, i, j;
+	int t, n, k, i, j, a[s], amount=0;
     scanf("%d", &t);
-
-    int a[n];
 
     for(i=0; i<t; i++)
     {
@@ -13,16 +12,18 @@ int main(void)
         for(j=0;j<n;j++)
         {
             scanf("%d", &a[j]);
-
             if(a[j]<k)
             {
-                printf("1\n");
+                amount = a[j];
+                printf("1");
+                amount = a[j] - a[j+1];
             }
             else
             {
-                printf("0\n");
+                printf("0");
             }
         }
+        printf("\n");
     }
         
     

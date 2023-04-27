08ed0de064ae401abffd0d405677069f76fa76f2
mdyasinahmed <yasinahmed7875@gmail.com>
2023-04-27 23:55:36 +0600

    programming languages problem code updated

--- (cc- LTIME99) - programming-languages.c
+++ (cc- LTIME99) - programming-languages.c
@@ -2,6 +2,15 @@
 
 int main(void)
 {
+    int t, a, b, a1, b1, a2, b2;
+    scanf("%d", &t);
+    
+    while(t--)
+    {
+        scanf("%d %d %d %d %d %d", &a, &b, &a1, &b1, &a2, &b2);
+
+        
+    }
 	
     
     return 0;

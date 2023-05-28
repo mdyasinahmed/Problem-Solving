e3bfed227b41ea707dbb1697e9d9505874d981a3
mdyasinahmed <yasinahmed7875@gmail.com>
2023-05-28 23:59:26 +0600

    equal integer problem solved

--- (bc)INCREAR-Equal_Integers.c
+++ (bc)INCREAR-Equal_Integers.c
@@ -7,8 +7,16 @@ int main()
 
     while(t--)
     {
+        int x, y;
+        scanf("%d %d", &x, &y);
         
+        int i;
+        for(i=0; i<(x!=y); i++){
+            x = x+1;
+            y = y+2;
+        }
 
+        printf("%d\n", i);
     }
 
     return 0;

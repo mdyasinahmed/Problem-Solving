da046a9b4f54ba948ca5f135f28b9bc597fe5538
mdyasinahmed <yasinahmed7875@gmail.com>
2023-06-29 02:53:59 +0600

    unsolved problem code added

--- (cf)Two Elevators.cpp
+++ (cf)Two Elevators.cpp
@@ -1,8 +1,21 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int a, b, c;
+        cin>>a>>b>>c;
+        if()
+        {
+            /* code */
+        }
+        
+    }
 
     return 0;
 }
+
+// https://codeforces.com/problemset/problem/1729/A

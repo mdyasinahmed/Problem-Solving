59fc771bc5de5ef3f298c20ccf287091e0c4a1ef
mdyasinahmed <yasinahmed7875@gmail.com>
2023-07-02 03:30:33 +0600

    Young Physicist problem solved code with link added

--- 100ProblemsChallenge/cf-young-physicist.cpp
+++ 100ProblemsChallenge/cf-young-physicist.cpp
@@ -8,20 +8,22 @@ int main()
     
     int a[n][3];
 
-    for(i=0;i<n;++i){
-        for(j=0;j<n;++j){
+    for(i=0;i<n;i++){
+        for(j=0;j<3;j++){
             cin>>a[i][j];
         }
     }
 
-    for(i=0;i<n;++i){
+    for(i=0;i<n;i++){
         x += a[i][0];
         y += a[i][1];
         z += a[i][2];
     }
 
-    if(x==0 && y==0 && z==0) cout<<"YES"<<endl;
+    if(x==y && y==z && x==z && x==0) cout<<"YES"<<endl;
     else cout<<"NO"<<endl;
 
     return 0;
 }
+
+// https://codeforces.com/problemset/problem/69/A
